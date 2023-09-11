0bcb96a8dc31cf93a8130329d53bb94886f1a432
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-09-11 11:32:44 +0530

    Added FB Video downloader (basic)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [fbVideoLink, setFbVideoLink] = useState("");
+  const [doApiCall, setDoApiCall] = useState(false);
+  const [resFbData, setResFbData] = useState("");
+
+  const submitHandler = () => {
+    // console.log(fbVideoLink);
+    setDoApiCall(!doApiCall);
+  };
+
+  useEffect(() => {
+    const getFbVideoData = async () => {
+      const response = await fetch(
+        `https://vdser.vercel.app/api/fb?video=${fbVideoLink}`
+      );
+
+      await setDoApiCall(!doApiCall);
+
+      // await console.log(response.ok);
+      const resData = await response.json();
+      console.log(resData);
+      setResFbData(resData);
+    };
+
+    if (doApiCall) {
+      getFbVideoData();
+    } else {
+      return; //
+    }
+  }, [fbVideoLink, doApiCall]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container color={"white"}>
+        <Text mb={10}>Facebook Video Downloader</Text>
+        <Text mb={10}>https://fb.watch/m_jEGodCb7</Text>
+        <FormControl mb={10}>
+          <FormLabel>FB Video Link (फेसबुक वीडियो लिंक)</FormLabel>
+          <Input
+            type="text"
+            mb={10}
+            value={fbVideoLink}
+            onChange={(e) => setFbVideoLink(e.target.value)}
+          />
+          <Button onClick={submitHandler}>Submit</Button>
+        </FormControl>
+
+        {resFbData && (
+          <>
+            <Text mb={6}>{resFbData.title}</Text>
+            <HStack justifyContent={"center"} gap={10}>
+              <Link
+                p={3}
+                backgroundColor={"white"}
+                borderRadius={5}
+                color={"#171717"}
+                fontWeight={"bold"}
+                target="_blank"
+                href={resFbData.hd}
+              >
+                Download in HD
+              </Link>
+              <Link
+                p={3}
+                backgroundColor={"white"}
+                borderRadius={5}
+                color={"#171717"}
+                fontWeight={"bold"}
+                target="_blank"
+                href={resFbData.sd}
+              >
+                Download in SD
+              </Link>
+            </HStack>
+          </>
+        )}
+      </Container>
     </div>
   );
 }
